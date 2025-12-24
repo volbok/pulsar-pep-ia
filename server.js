aa754cc181ddc6b28f960a6140e81d78b465f068
volbok <rodrigocarvalholessa@gmail.com>
2025-12-23 22:12:51 -0300

    AJUSTES EM IA EVOLUÇÕES.

--- server.js
+++ server.js
@@ -465,7 +465,7 @@ app.post("/quickmedpersonal", async (req, res) => {
           - quaisquer outros campos do modelo do usuário que representem listas clínicas
 
           Exemplo de conteúdo válido para um campo do tipo string com múltiplos itens:
-          "Exemplo de item 1.\nExemplo de item 2.\nExemplo de item 3."
+          "Exemplo de item 1\nExemplo de item 2\nExemplo de item 3"
 
         ---
 
